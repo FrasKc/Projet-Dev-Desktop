52861f94c9633da949a21b223964f55c7c79f1f1
Célian Frasca <celian.frasca@gmail.com>
2023-06-26 16:24:11 +0200

    Test sur la sauvegarde

--- src/test/java/ActivityControllerImplTest.java
+++ src/test/java/ActivityControllerImplTest.java
@@ -12,6 +12,7 @@ import java.sql.Date;
 import java.time.Instant;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -26,6 +27,7 @@ public class ActivityControllerImplTest {
             330,
             5
     );
+    String id = "idActivity";
 
     @BeforeEach
     public void setUp() {
@@ -36,5 +38,19 @@ public class ActivityControllerImplTest {
     public void calculate_load() {
         double load = classUnderTest.calculateLoad(activity.getDuration(), activity.getRpe());
         assertThat(load).isEqualTo(27.50);
+    }
+
+    @Test
+    public void save_withActivity_shouldCallRepository() {
+        //Given
+        when(activityRepository.save(activity)).thenReturn(id);
+
+        //When
+        String result = classUnderTest.saveActivity(activity);
+
+        //Then
+        verify(activityRepository).save(activity);
+        verify(activityRepository).save(any(Activity.class));
+        verify(activityRepository, times(1)).save(any(Activity.class));
     }
 }
