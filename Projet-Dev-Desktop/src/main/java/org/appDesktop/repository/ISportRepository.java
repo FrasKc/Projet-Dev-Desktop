e7eb5397afb60a6738bf16571f638513fe303339
Célian Frasca <celian.frasca@gmail.com>
2023-06-26 12:17:54 +0200

    Refactor

--- Projet-Dev-Desktop/src/main/java/org/appDesktop/repository/ISportRepository.java
+++
@@ -1,9 +0,0 @@
-package org.appDesktop.repository;
-
-import com.mongodb.client.result.InsertOneResult;
-import org.appDesktop.model.Activity;
-
-public interface ISportRepository {
-
-    InsertOneResult save(Activity activity);
-}
